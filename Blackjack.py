42dad84de2cdcaf74184cd1de7bb59fad0b16bf3
Стас Гусельников <54426260+TheBoogeymanPWNZ@users.noreply.github.com>
2020-08-25 12:14:03 +0700

    Added initial issue of 2 cards

--- Blackjack.py
+++ Blackjack.py
@@ -3,22 +3,25 @@ import random
 hand = [6, 7, 8, 9, 10, 2, 3, 4, 11] * 4
 random.shuffle(hand)
 
-print ('You wanna play?')
-count = 0
+a = current = hand.pop()
+x = current = hand.pop()
+print('You wanna play?')
+count = a + x
+print('You were given two cards in total', a, 'and', x)
 
 while True:
-    choice = input('Need another card? y/n')
+    choice = input('Need another card? y/n \n')
     if choice == 'y':
         current = hand.pop()
-        print('You got the card %d' %current)
+        print('You got the card %d' % current)
         count += current
         if count > 21:
-            print('You Lose')
+            print('You typed %d points and lost' % count)
         elif count == 21:
             print('You have blackjack')
             break
         else:
-            print('You have %d points' %count)
+            print('You have %d points' % count)
     elif choice == 'n':
-        print('Finished the game with %d points' %count)
+        print('Finished the game with %d points' % count)
         break
