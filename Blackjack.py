473a1834eb213a38fe084f7b8e30f781caa0c006
Стас Гусельников <54426260+TheBoogeymanPWNZ@users.noreply.github.com>
2020-08-27 13:55:03 +0700

    Fixed a bug in the calculation of winnings in blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -7,25 +7,25 @@ a = current = hand.pop()
 x = current = hand.pop()
 print('You wanna play?')
 count = a + x
+croupier = a + x
 bet = int(input('Place your bet ₽: '))
 print('You were given two cards in total', a, 'and', x)
-
 while count <= 21:
     choice = input('Need another card? y/n \n')
+    if count == 21:
+        print('You have blackjack!!!')
+        print('Your winnings: ', bet * 1.5)
+        break
     if choice == 'y':
         current = hand.pop()
         print('You got the card %d' % current)
         count += current
         if count > 21:
             print('You have scored %d points' % count)
-            print('Your bet is lost:', bet)
-        elif count == 21:
-            print('You have blackjack!!!')
-            print('Your winnings: ', bet * 1,5)
-            break
+            print('--- Your bet is lost:', bet)
         else:
             print('You have %d points' % count)
     elif choice == 'n':
         print('Finished the game with %d points' % count)
-        print('Your winnings: ', bet * 2)
+        print('+++ Your winnings: ', bet * 2)
         break
