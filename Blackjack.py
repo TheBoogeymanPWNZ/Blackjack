a1af6098698922c31e9be6f126355ad3ad0d375d
Стас Гусельников <54426260+TheBoogeymanPWNZ@users.noreply.github.com>
2020-09-01 22:15:20 +0700

    Fixed re-bet bug and removed crutch

--- Blackjack.py
+++ Blackjack.py
@@ -26,24 +26,23 @@ while bank >= 0:
     player = None
     croupier = None
     player = a + x
-    croupier = b + c
+    croupier = b
     bet = None
     bet = int(input('Place your bet ₽: '))
+    bank -= bet
     if bank < bet:
         print('You bet more than you contributed')
     elif bank >= bet:
         print('You were given two cards in total', a, learA, 'and', x, learX)
-        print('The dealer took two cards, one of which', b, learB)
+        print('The dealer took two cards, one of which', b, learB, c, learC)
 
         if player == 21:
             print('You have blackjack!!!')
             print('Your winnings: ', bet)
-            bank -= bet  # This is a crutch due to a re-bet error
             bank += bet * 1.5
         elif player > 21:
             print('You have scored %d points' % player)
             print('--- Your bet is lost:', bet)
-            bank -= bet
 
         while player < 21:
             if croupier >= 17:
@@ -55,17 +54,16 @@ while bank >= 0:
                 if player > 21:
                     print('You have scored %d points' % player)
                     print('--- Your bet is lost:', bet)
-                    bank -= bet
                 elif player == 21:
                     print('Finished the game with %d points' % player)
                     print('+++ Your winnings: ', bet * 2)
-                    bank -= bet
                     bank += bet * 2
                 else:
                     print('You have %d points' % player)
             elif choice == 'n':
                 print('The dealer reveals the second card', b, learB, 'and', c, learC)
             while choice == 'n':
+                croupier += c
                 if croupier < 17:
                     current = hand.pop()
                     print('The dealer draws a card %d ' % current, lear)
@@ -74,7 +72,6 @@ while bank >= 0:
                     print('The dealer is too busy %d' % croupier)
                     print('Finished the game with %d points' % player)
                     print('+++ Your winnings: ', bet * 2)
-                    bank -= bet
                     bank += bet * 2
                     break
                 elif player == croupier:
@@ -85,11 +82,9 @@ while bank >= 0:
                 elif player < croupier:
                     print('The dealer scored %d' % croupier)
                     print('--- Your bet is lost:', bet)
-                    bank -= bet
                     break
                 elif player > croupier:
                     print('Finished the game with %d points' % player)
                     print('+++ Your winnings: ', bet * 2)
-                    bank -= bet
                     bank += bet * 2
                     break
